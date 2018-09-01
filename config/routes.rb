dc0fc63987fc270dfa7dda4c7e7ccd61f98c8e00
Mukunda Gogoi <mukundamg1993@gmail.com>
2018-09-01 12:12:49 +0530

    revert routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  #mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 
   get 'home/index'
   get 'profile/:username' => 'profile#index'
